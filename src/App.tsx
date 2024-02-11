abc2e4aa774d9d1e86b4dd5926e5f6cc4a597e30
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-02-11 06:15:05 +0530

    fix(icon/favicon): Modified icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import { ModeToggle } from "./components/ModeToggle";
 import { ParseDataDialog } from "./components/ParseDataInput";
 import { useTriggerRefresh } from "./stores/StateStore";
 import { ResetDialog } from "./components/ResetDialog";
+import Image from "next/image";
 
 function App() {
   const { refresh } = useTriggerRefresh();
   return (
     <main>
       <nav className="fixed flex items-center justify-between w-full px-6 py-4">
-        <h1 className="text-3xl font-bold">🧮</h1>
+        <h1 className="text-3xl font-bold relative flex-shrink-0">
+          <div className="w-12 h-12 bg-yellow-200/10 -left-[6px] -top-[6px] absolute rounded-full blur-lg" />
+          <Image
+            src="/android-chrome-192x192.png"
+            alt="Kamino Logo"
+            width={36}
+            height={36}
+            priority
+            loading="eager"
+            className="relative"
+          />
+        </h1>
         <div className="flex items-center gap-2">
           <div className="relative inline-flex group">
             <div className="absolute transition-all duration-500 inset-full bg-gradient-to-tr from-sky-600/30 via-pink-600/30 to-orange-600/30 rounded blur-lg group-hover:-inset-px" />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,23 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://api.fontshare.com/v2/css?f[]=general-sans@1&display=swap"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </head>
       <body>
         <div id="root">{children}</div>
